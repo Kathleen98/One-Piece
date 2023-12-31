142020f332dee698a6625d6efd499420e7dae550
Kathleen98 <99849068+Kathleen98@users.noreply.github.com>
2023-12-30 23:49:59 -0300

    Seção timer

--- src/js/wanted.js
+++ src/js/wanted.js
@@ -1,30 +1,46 @@
-// const containerImages = document.querySelector('.wanted__slider__img');
+const containerImages = document.querySelector('.wanted__container');
 
-// async function searchShowImages () {
-//   try{
-//     const search = await fetch("http://localhost:3000/images");
-//     const images = await search.json();
+async function searchShowImages () {
+  try{
+    const search = await fetch("http://localhost:3000/images");
+    const images = await search.json();
 
-//       images.forEach((image) =>{
-//         if(image.url == ""){
-//           throw new Error('Imagem sem url cadastrada')
-//         }
+      images.forEach((image, index) =>{
+        if(image.url == ""){
+          throw new Error('Imagem sem url cadastrada')
+        }
         
-//         const imgElement = document.createElement('img');
-//         imgElement.src = image.src;
-//         imgElement.alt = image.name;
-//         imgElement.classList.add('wanted__slider__img__item');
-//         containerImages.appendChild(imgElement);
+        const imgList = document.createElement('li');
+        imgList.classList.add('wanted__container__list')
 
-//       })
+        const imgElement = document.createElement('img');
+        imgElement.src = image.src;
+        imgElement.alt = image.name;
+        imgElement.classList.add('wanted__container__list__img');
+        imgList.appendChild(imgElement);
 
-//   }catch(error) {
-//     containerImages.innerHTML = `<p>Houve um erro ao carregas as imagens: ${error}</p>`
-//   }
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = image.name;
+        titleElement.classList.add('wanted__container__list__title');
+        imgList.appendChild(titleElement);
 
-// }
+        const textElement = document.createElement('p');
+        textElement.textContent = image.description;
+        textElement.classList.add('wanted__container__list__text');
+        imgList.appendChild(textElement);
+
+        containerImages.appendChild(imgList);
+        
+
+      })
+
+  }catch(error) {
+    containerImages.innerHTML = `<p>Houve um erro ao carregas as imagens: ${error}</p>`
+  }
+
+}
 
 
-// searchShowImages()
+searchShowImages()
 
   
