24aab85af210cb1e1fdfdeef2d41ae5a92f257dc
Kathleen98 <99849068+Kathleen98@users.noreply.github.com>
2024-01-03 19:00:00 -0300

    Footer js

---
+++ src/js/footer.js
@@ -0,0 +1,21 @@
+const footerElement = document.querySelector('.footer');
+
+footerElement.innerHTML = `
+        <img class="footer__logo" src="https://i.pinimg.com/564x/3f/c8/6b/3fc86bd3ab6b3e7a632d6ab3e9326c93.jpg" alt="Caveira do bando Chapeu de Palha">
+        <div class="footer__texts">
+            <p>
+                "Em um mundo de Grandes Linhas, descubra a sua própria história navegando conosco. O tesouro está na jornada, não apenas no destino. Yohoho!"
+            </p>
+            <p>
+                &copy;Copyright - 2023 | Desenvolvido por Kathleen Santos
+            </p>
+        </div>
+        <div class="footer__socials">
+            <a class="footer__socials__link" href="https://www.linkedin.com/in/kathleenc-santos/" target="_blank">
+                <img class="footer__socials__link__img" src="https://i.pinimg.com/564x/05/da/34/05da3431471058c803afacb2f5483f7b.jpg" alt="LinkedIn">
+            </a>
+            <a class="footer__socials__link" href="https://github.com/Kathleen98" target="_blank">
+                <img class="footer__socials__link__img" src="https://i.pinimg.com/564x/66/71/3a/66713a96b9b21dffd3a85a5d748a3171.jpg" alt="Github">
+            </a>
+        </div>
+    `
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,69 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Função para menu hamburguer responsivo
     const hamburguer = document.querySelector('.hero__hamburguer');
     const hero = document.querySelector('.hero__container__list');
   
     hamburguer.addEventListener('click', function () {
       hero.classList.toggle('hero__container__list--active-menu');
     });
+
+    // Seção mares One Piece
+    const worldElement = document.querySelector('.world');
+    const endpointAPI = "http://localhost:3000/world";
+    let seas = [];
+
+    async function searchWorld() {
+        try {
+            const search = await fetch(endpointAPI);
+            if (!search.ok) {
+                throw new Error(`HTTP error! Status: ${search.status}`);
+            }
+            seas = await search.json();
+            displaySeas(seas);
+        } catch (error) {
+            const erro = document.createElement('p');
+            erro.textContent = `Houve um erro no carregamento ${error}`
+        }
+    }
+
+    searchWorld();
+
+    function displaySeas(listSeas) {
+        listSeas.forEach(sea => {
+            worldElement.innerHTML += `
+            <div class="world__container">
+                <img class="world__container__img" src="${sea.image}" alt="North Blue">
+                <h2 class="world__container__title">
+                    ${sea.name}
+                </h2>
+                <p class="world__container__text">
+                    ${sea.paragrafy}
+                </p>
+                <a class="world__container__link" href="${sea.link}">Ver mais</a>
+            </div>
+            `;
+        });
+    }
+
+
+    // Seção Akuma No Mi
+    const frutElement = document.querySelector('.akumaNoMi');
+
+    frutElement.innerHTML = `
+    <h2 class="akumaNoMi__title">
+    Akuma No Mi
+    </h2>
+    <img class="akumaNoMi__img" src="https://i.pinimg.com/564x/4b/72/83/4b7283da90cf705abd700c1f752342e5.jpg" alt="Akuma No Mi">
+    <div class="akumaNoMi__texts">
+      <p class="akumaNoMi__texts__item">
+          Vamos explorar o mistério das Akuma No Mi, essas frutas especiais e enigmáticas que pontilham nosso vasto mundo, prometendo poderes sobrenaturais permanentes para aqueles corajosos o suficiente para dar uma mordida. Mas, como tudo na vida, há um preço a se pagar, e as Akuma No Mi não são exceção!
+      </p>
+      <p class="akumaNoMi__texts__item">
+          Essas frutas concedem habilidades sobre-humanas, transformando aqueles que as consomem em verdadeiros titãs dos mares. Contudo, como toda boa história, há uma reviravolta. Os afortunados (ou talvez desafortunados) consumidores dessas frutas ficam permanentemente impedidos de controlar poderes que possam causar destruição em massa. Criar terremotos ou desencadear caos massivo se torna uma habilidade inalcançável.
+      </p>
+    </div>
+    <a class="akumaNoMi__link" href="">Ver mais</a>
+    `
+
   });
